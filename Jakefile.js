5b3283eb06e8af486f08f30d459b7cce191e5899
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-13 17:46:42 +0100

    Get expected Node version from package.json

--- Jakefile.js
+++ Jakefile.js
@@ -1,26 +1,27 @@
 (function() {
   "use strict";
 
-  const EXPECTED_NODE_VERSION = "v8.5.0";
-
   desc("Default build");
   task("default", [ "version" ], function() {
     console.log("Hello, I'm the default task.");
     console.log("\n\nBUILD OK!");
   });
-
+  
   desc("Pure ridiculosity");
   task("gooble", function() {
     console.log("Gooble, gooble!");
   });
-
+  
   desc("Check Node version")
   task("version", function() {
     console.log("Checking Node version: .");
-
+    
+    const packageJson = require("./package.json");
+    const expectedVersion = "v" + packageJson.engines.node;
     const actualVersion = process.version;
-    if (actualVersion !== EXPECTED_NODE_VERSION) {
-      fail(`Incorrect Node version: expected ${EXPECTED_NODE_VERSION}, but was ${actualVersion}.`);
+
+    if (actualVersion !== expectedVersion) {
+      fail(`Incorrect Node version: expected ${expectedVersion}, but was ${actualVersion}.`);
     }
   });
 }());
