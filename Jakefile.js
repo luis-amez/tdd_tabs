951904d5e5d21758d50e0679948eb5b874ed9003
Luis Amez <luis.amez.vitoria@gmail.com>
2018-03-13 17:40:36 +0100

    Add script that checks Node version

--- Jakefile.js
+++ Jakefile.js
@@ -1,8 +1,10 @@
 (function() {
   "use strict";
 
+  const EXPECTED_NODE_VERSION = "v8.5.0";
+
   desc("Default build");
-  task("default", function() {
+  task("default", [ "version" ], function() {
     console.log("Hello, I'm the default task.");
     console.log("\n\nBUILD OK!");
   });
@@ -10,5 +12,15 @@
   desc("Pure ridiculosity");
   task("gooble", function() {
     console.log("Gooble, gooble!");
+  });
+
+  desc("Check Node version")
+  task("version", function() {
+    console.log("Checking Node version: .");
+
+    const actualVersion = process.version;
+    if (actualVersion !== EXPECTED_NODE_VERSION) {
+      fail(`Incorrect Node version: expected ${EXPECTED_NODE_VERSION}, but was ${actualVersion}.`);
+    }
   });
 }());
